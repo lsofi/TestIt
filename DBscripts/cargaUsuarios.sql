00c0929f7a2df34e27ac3ee0e501c648c7ed19a1
Joaquin Neschisi <j.neschisi@gmail.com>
2020-08-25 13:33:31 -0300

    Login funcionando2

---
+++ DBscripts/cargaUsuarios.sql
@@ -0,0 +1,13 @@
+USE [TestIt]
+
+INSERT INTO Usuarios
+VALUES (1, 'JNeschisi', 1234, 0)
+
+INSERT INTO Usuarios
+VALUES (2, 'GCarranza', 1234, 0)
+
+INSERT INTO Usuarios
+VALUES (3, 'ACornejo', 1234, 0)
+
+INSERT INTO Usuarios
+VALUES (4, 'SJuarez', 1234, 0)
---
+++ TestIt/Logica/GestorUsuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIt.Entidades;
+using TestIt.Datos;
+
+namespace TestIt.Logica
+{
+    public class GestorUsuario
+    {
+        private UsuarioDB oUsuarioDB;
+        public GestorUsuario()
+        {
+            oUsuarioDB = new UsuarioDB();
+        }
+        public IList<Usuario> ObtenerTodos()
+        {
+            return oUsuarioDB.GetAll();
+        }
+
+        public Usuario BuscarUsuario(string usuario)
+        {
+            return oUsuarioDB.GetUser(usuario);
+        }
+    }
+}
