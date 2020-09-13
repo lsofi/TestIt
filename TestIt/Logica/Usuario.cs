4c8a73452c5cf132dc99f8c7a292890c5e401821
Joaquin Neschisi <j.neschisi@gmail.com>
2020-09-13 16:19:35 -0300

    solucionError

---
+++ TestIt/Logica/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIt.Datos;
+
+namespace TestIt.Logica
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+
+        public static Usuario buscarUsuario(string nomUsuario)
+        {
+            UsuarioDao ud = new UsuarioDao();
+            return ud.BuscarUsuario(nomUsuario);
+        }
+
+        public bool ValidarContraseña(string password)
+        {
+            return (Contraseña != null && Contraseña.Equals(password));
+        }
+
+        public override string ToString()
+        {
+            return NombreUsuario;
+        }
+    }
+}
